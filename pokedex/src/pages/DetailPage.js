f22aeda0946b7ba57914f62a310b590abe6e13ad
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2022-05-03 19:23:43 -0300

    requisicoes de API feitas

--- pokedex/src/pages/DetailPage.js
+++ pokedex/src/pages/DetailPage.js
@@ -1,17 +1,37 @@
 import React, { useState, useEffect } from "react";
 // import styled from "styled-components";
-// import axios from "axios";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { goToListTripPage, goToLogin } from "../routes/coordinator.js";
+import { goToPokeDexPage } from "../routes/coordinator.js";
+import { type } from "@testing-library/user-event/dist/type";
 
 const DetailPage = () => {
     const navigate = useNavigate()
 
+    useEffect(()=>{
+        getPokemonDetail()
+    }, [])
+
+    const getPokemonDetail = () => {
+        const urlDetailPokemon = 'https://pokeapi.co/api/v2/pokemon/:name/'
+        axios
+        .get(urlDetailPokemon)
+        .then((response) => {
+            console.log(response.data)
+            // console.log(response.data.types)
+            // console.log(response.data.stats)
+            // console.log(response.data.moves)
+            // console.log(response.data.sprites)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div>
             <p>Detail Page</p>
-            <button>Voltar</button>
-            <button>Ir para PokeDex</button>
+            <button onClick={() => goToPokeDexPage(navigate)}>Visualizar PokeDex</button>
         </div>
     )
 }
