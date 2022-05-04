2bcc4e04adaba9d6adc8d0fedc93aa73069ad80e
Rodrigo Pernambuco <rodrigoperera1@hotmail.com>
2022-05-04 15:51:31 -0300

    logica detailpage

--- pokedex/src/pages/DetailPage.js
+++ pokedex/src/pages/DetailPage.js
@@ -1,36 +1,80 @@
 import React, { useState, useEffect } from "react";
 // import styled from "styled-components";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { goToPokeDexPage } from "../routes/coordinator.js";
-import { type } from "@testing-library/user-event/dist/type";
 
 const DetailPage = () => {
+
+    const [pokemon, setPokemon] = useState({sprites: {}, stats: [], types: [], moves: []})
+    
     const navigate = useNavigate()
+
+    const { id } = useParams()
 
     useEffect(()=>{
         getPokemonDetail()
     }, [])
 
     const getPokemonDetail = () => {
-        const urlDetailPokemon = 'https://pokeapi.co/api/v2/pokemon/:name/'
+        const urlDetailPokemon = `https://pokeapi.co/api/v2/pokemon/${id}/`
         axios
         .get(urlDetailPokemon)
         .then((response) => {
             console.log(response.data)
-            // console.log(response.data.types)
-            // console.log(response.data.stats)
-            // console.log(response.data.moves)
-            // console.log(response.data.sprites)
+            setPokemon(response.data)
         })
         .catch((error) => {
             console.log(error)
         })
     }
 
+    const fetchPokemonStatus = pokemon.stats.map((status) => {
+        return (
+            <div>
+                <p><b>{status.stat.name.toUpperCase()}</b>: {status.base_stat}</p>
+            </div>
+        )
+    })
+
+    const fetchPokemonTypes = pokemon.types.map((eachtype) => {
+        return (
+            <div>
+                <p><b>{eachtype.type.name.toUpperCase()}</b></p>
+            </div>
+        )
+    })
+
+    const fetchPokemonMoves = pokemon.moves.slice(77, 81).map((eachmove) => {
+        return (
+            <div>
+                <p><b>{eachmove.move.name.toUpperCase()}</b></p>
+            </div>
+        )
+    })
+
     return (
         <div>
-            <p>Detail Page</p>
+            <h1>Detail Page</h1>
+            <div>
+                <img src={pokemon.sprites.front_default} alt="pokemon de frente" />
+                <img src={pokemon.sprites.back_default} alt="pokemon de costas" />
+            </div>
+
+            <div>
+                <h2><i>Stats:</i></h2>
+                {fetchPokemonStatus}
+            </div>
+
+            <div>
+                <h2><i>Tipo:</i></h2>
+                {fetchPokemonTypes}
+            </div>
+
+            <div>
+                <h2><i>Moves:</i></h2>
+                {fetchPokemonMoves}
+            </div>
             <button onClick={() => goToPokeDexPage(navigate)}>Visualizar PokeDex</button>
         </div>
     )
